7c11def87c05a4746cd80d7e2ac67657a731177a
Mayank Reddy Ankilla <79315030+qwertymra@users.noreply.github.com>
2023-10-13 10:42:28 +0530

    Update hinglish.py

--- hinglish.py
+++ hinglish.py
@@ -3,11 +3,9 @@ from googletrans import Translator
 def translate_to_hinglish(text):
     translator = Translator()
     translation = translator.translate(text, src='en', dest='hi')
-    # Replace specific words with English words in the translation
     hinglish_translation = translation.text.replace('टिप्पणी अनुभाग', 'comment section').replace('प्रतिक्रिया', 'feedback')
     return hinglish_translation
 
-# Example usage
 english_text = "Definitely share your feedback in the comment section."
 hinglish_text = translate_to_hinglish(english_text)
 print("Input (English):", english_text)
